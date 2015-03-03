a84b28972b0641f00dedcb6d4072bcd712fb192e
hyunbin <hyunbip@gmail.com>
2015-03-02 21:36:46 -0600

    Initial support for the support v7 app bar widget

--- app/src/main/java/cashpa/bettermtd/MainActivity.java
+++ app/src/main/java/cashpa/bettermtd/MainActivity.java
@@ -1,17 +1,25 @@
 package cashpa.bettermtd;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Hello world!");
+        toolbar.setTitleTextColor(-1);
+        setSupportActionBar(toolbar);
     }
 
 
